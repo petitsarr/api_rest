dfa6cd07240b1d6dbeb8e5cd21f69cd7edf4714a
petit <fayembad@gmail.com>
2021-11-13 00:40:42 +0100

    schema

--- models/StufffModel.js
+++ models/StufffModel.js
@@ -3,6 +3,26 @@ import mongoose from "mongoose" ;
 const stuff = mongoose.Schema  ;
 
 const stuff = new stuff({
+    title :{
+        type : String ,
+        required : true 
+    } ,
+    description : {
+        type : String ,
+        required : true
+    } ,
+    imageUrl :{
+        type : String ,
+        required : true
+    } ,
+    price :{
+        type : String ,
+        required : true
+    } ,
+    userId : {
+        type : String ,
+        required : true
+    }
 
 })
 
