a5d1397d851f48ffac9be41aa0c512969b1feeb5
petit <fayembad@gmail.com>
2021-11-15 14:30:46 +0100

    hash_password

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,11 +1,37 @@
 import mongoose from "mongoose" ;
-import User from  "../models/User"
+import User from  "../models/User" ;
+import bcrypt from "bcrypt" ;
 
 const Myuser = mongoose.model("Myuser",User)
 
 
-const inscription = async()=> {
+const inscription = async(req,res)=> {
+try {
 
+    const {email ,password} = req.body ;
+
+    const passhash =  await bcrypt(password,10) ; 
+  
+    const newuser = new Myuser({
+        email ,
+        password : passhash  
+    })
+    await newuser.save()
+    if (newuser) {
+    res.status(201).json({
+        message : "User creé avec succés"
+    })
+    }
+    else {
+        throw new Error("user non crée ")
+        
+    }
+    
+} catch (error) {
+    res.status(400).send(error)
+    
+}
+   
 }
 
 
